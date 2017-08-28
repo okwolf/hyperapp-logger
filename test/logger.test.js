e4392b5ea953cd87c3793e12c01011981b6063fd
Jorge Bucaran <mail@jorgebucaran.com>
2017-08-28 10:12:20 +0900

    Add tests & update instructions.
    - Add tests with 100% coverage.
    - Improve instructions, match style used in hyperapp docs.
    - Use prettier to format code.

---
+++ test/logger.test.js
@@ -0,0 +1,69 @@
+import { h, app } from "hyperapp"
+import logger from "../"
+
+const defaultConsole = console
+
+afterEach(() => {
+  console = defaultConsole
+})
+
+test("custom log function", done => {
+  app({
+    state: {
+      value: 0
+    },
+    actions: {
+      up(state) {
+        return { value: state.value + 1 }
+      },
+      upWithThunk(state, actions, data) {
+        return update => update({ value: state.value + data })
+      }
+    },
+    events: {
+      load(state, actions) {
+        actions.up()
+        actions.upWithThunk(1)
+      }
+    },
+    mixins: [
+      logger({
+        log(state, action, nextState) {
+          if (action.name === "up") {
+            expect(state).toEqual({ value: 0 })
+            expect(nextState).toEqual({ value: 1 })
+          } else if (action.name === "upWithThunk") {
+            expect(state).toEqual({ value: 1 })
+            expect(action.data).toBe(1)
+            expect(nextState).toEqual({ value: 2 })
+          }
+          done()
+        }
+      })
+    ]
+  })
+})
+
+test("log", done => {
+  console = {
+    log() {},
+    group() {},
+    groupEnd() {
+      done()
+    }
+  }
+
+  app({
+    actions: {
+      foo(state) {
+        return state
+      }
+    },
+    events: {
+      load(state, actions) {
+        actions.foo()
+      }
+    },
+    mixins: [logger()]
+  })
+})
