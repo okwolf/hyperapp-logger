ae52dd8fb6f58f4f00d4776a94c6c21327ad9385
Wolfgang <wolf@okwolf.com>
2017-10-04 22:04:37 -0700

    out with the mixins, in with the HOAs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,49 @@
-function log(prevState, action, nextState) {
-  console.group('%c action', 'color: gray; font-weight: lighter;', action.name);
-  console.log('%c prev state', 'color: #9E9E9E; font-weight: bold;', prevState);
-  console.log('%c data', 'color: #03A9F4; font-weight: bold;', action.data);
-  console.log('%c next state', 'color: #4CAF50; font-weight: bold;', nextState);
-  console.groupEnd();
+function defaultLog(prevState, action, nextState) {
+  console.group("%c action", "color: gray; font-weight: lighter;", action.name)
+  console.log("%c prev state", "color: #9E9E9E; font-weight: bold;", prevState)
+  console.log("%c data", "color: #03A9F4; font-weight: bold;", action.data)
+  console.log("%c next state", "color: #4CAF50; font-weight: bold;", nextState)
+  console.groupEnd()
 }
 
 export default function(options) {
-  options = options || {};
-  options.log = typeof options.log === 'function' ? options.log : log;
+  options = options || {}
+  options.log = typeof options.log === "function" ? options.log : defaultLog
 
-  return function(emit) {
-    var actionStack = [];
-    return {
-      events: {
-        action: function(state, actions, action) {
-          actionStack.push(action);
-        },
-        resolve: function(state, actions, result) {
-          if (typeof result === 'function') {
-            var action = actionStack.pop();
-            return function(update) {
-              return result(function(result) {
-                actionStack.push(action);
-                return update(result);
-              });
-            };
-          }
-        },
-        update: function(state, actions, nextState) {
-          options.log(state, actionStack.pop(), nextState);
-        }
+  return function(app) {
+    return function(props) {
+      function enhance(actions) {
+        return Object.keys(actions).reduce(function(otherActions, name) {
+          const action = actions[name]
+          otherActions[name] =
+            typeof action === "function"
+              ? function(state, actions, data) {
+                  const result = action(state, actions, data)
+                  if (typeof result === "function") {
+                    return function(update) {
+                      return result(function(withState) {
+                        options.log(
+                          state,
+                          { name: name, data: data },
+                          withState
+                        )
+                        return update(withState)
+                      })
+                    }
+                  } else {
+                    options.log(state, { name: name, data: data }, result)
+                    return result
+                  }
+                }
+              : enhance(action)
+          return otherActions
+        }, {})
       }
-    };
-  };
+
+      props.actions = enhance(props.actions)
+      const appActions = app(props)
+
+      return appActions
+    }
+  }
 }
