682bd2cf371936f92a374c810c67f1d646168ff8
Wolfgang <wolf@okwolf.com>
2017-10-15 09:33:59 -0700

    namespace prefixes for state slice actions

--- test/logger.test.js
+++ test/logger.test.js
@@ -7,41 +7,6 @@ afterEach(() => {
   console = defaultConsole
 })
 
-test("custom log function", done =>
-  logger({
-    log(state, action, nextState) {
-      if (action.name === "up") {
-        expect(state).toEqual({ value: 0 })
-        expect(nextState).toEqual({ value: 1 })
-      } else if (action.name === "upWithThunk") {
-        expect(state).toEqual({ value: 1 })
-        expect(action.data).toBe(1)
-        expect(nextState).toEqual({ value: 2 })
-        done()
-      }
-    }
-  })(app)({
-    state: {
-      slice: {
-        value: 0
-      }
-    },
-    actions: {
-      slice: {
-        up(state) {
-          return { value: state.value + 1 }
-        },
-        upWithThunk(state, actions, data) {
-          return update => update({ value: state.value + data })
-        }
-      }
-    },
-    init(state, actions) {
-      actions.slice.up()
-      actions.slice.upWithThunk(1)
-    }
-  }))
-
 test("log", done => {
   console = {
     log() {},
@@ -62,3 +27,71 @@ test("log", done => {
     }
   })
 })
+
+test("custom log function", done =>
+  logger({
+    log(state, action, nextState) {
+      expect(state).toEqual({ value: 0 })
+      expect(action).toEqual({ name: "inc", data: { by: 2 } })
+      expect(nextState).toEqual({ value: 2 })
+      done()
+    }
+  })(app)({
+    state: {
+      value: 0
+    },
+    actions: {
+      inc: (state, actions, { by }) => ({ value: state.value + by })
+    },
+    init(state, actions) {
+      actions.inc({ by: 2 })
+    }
+  }))
+
+test("state slices", done =>
+  logger({
+    log(state, action, nextState) {
+      switch (action.name) {
+        case "hello":
+          expect(state).toEqual({ slice: { value: 0 } })
+          expect(nextState).toEqual({ message: "hello" })
+          break
+        case "slice.up":
+          expect(state).toEqual({ value: 0 })
+          expect(nextState).toEqual({ value: 1 })
+          break
+        case "slice.upWithThunk":
+          expect(state).toEqual({ value: 1 })
+          expect(action.data).toBe(1)
+          expect(nextState).toEqual({ value: 2 })
+          done()
+          break
+        default:
+          throw new Error(`Unexpected action: ${action.name}`)
+      }
+    }
+  })(app)({
+    state: {
+      slice: {
+        value: 0
+      }
+    },
+    actions: {
+      hello(state) {
+        return { message: "hello" }
+      },
+      slice: {
+        up(state) {
+          return { value: state.value + 1 }
+        },
+        upWithThunk(state, actions, data) {
+          return update => update({ value: state.value + data })
+        }
+      }
+    },
+    init(state, actions) {
+      actions.hello()
+      actions.slice.up()
+      actions.slice.upWithThunk(1)
+    }
+  }))
