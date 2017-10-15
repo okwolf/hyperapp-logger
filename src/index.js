9c8f9770bad26a2d5cdd74bf489e96e600403861
Wolfgang <wolf@okwolf.com>
2017-10-14 21:42:40 -0700

    removing middleware for now, causes complications with the bundle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { enhance, makeUpdate } from "hyperapp-middleware"
-
 function defaultLog(prevState, action, nextState) {
   console.group("%c action", "color: gray; font-weight: lighter;", action.name)
   console.log("%c prev state", "color: #9E9E9E; font-weight: bold;", prevState)
@@ -12,12 +10,40 @@ export default function(options) {
   options = options || {}
   options.log = typeof options.log === "function" ? options.log : defaultLog
 
-  return enhance(
-    makeUpdate(function(action) {
-      return function(prevState, actions, nextState) {
-        options.log(prevState, action, nextState)
-        return nextState
+  return function(app) {
+    return function(props) {
+      function enhance(actions) {
+        return Object.keys(actions).reduce(function(otherActions, name) {
+          const action = actions[name]
+          otherActions[name] =
+            typeof action === "function"
+              ? function(state, actions, data) {
+                  const result = action(state, actions, data)
+                  if (typeof result === "function") {
+                    return function(update) {
+                      return result(function(withState) {
+                        options.log(
+                          state,
+                          { name: name, data: data },
+                          withState
+                        )
+                        return update(withState)
+                      })
+                    }
+                  } else {
+                    options.log(state, { name: name, data: data }, result)
+                    return result
+                  }
+                }
+              : enhance(action)
+          return otherActions
+        }, {})
       }
-    })
-  )
+
+      props.actions = enhance(props.actions)
+      const appActions = app(props)
+
+      return appActions
+    }
+  }
 }
