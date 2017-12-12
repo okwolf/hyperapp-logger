8abc8b879454f34ec23a7d2df9b7ff931d042b8b
Wolfgang <wolf@okwolf.com>
2017-12-11 22:19:06 -0800

    updating for ridiculous new api

--- src/index.js
+++ src/index.js
@@ -5,39 +5,47 @@ export default function(options) {
   options.log = typeof options.log === "function" ? options.log : defaultLog
 
   return function(app) {
-    return function(props, container) {
-      function enhanceActions(actions, prefix) {
+    return function(model, view, container) {
+      function withoutFunctions(value) {
+        return Object.keys(value || {}).reduce(function(otherValues, name) {
+          var slice = value[name]
+          if (typeof slice !== "function") {
+            otherValues[name] =
+              Object(slice) === slice ? withoutFunctions(slice) : slice
+          }
+          return otherValues
+        }, {})
+      }
+      function enhanceActions(model, prefix) {
         var namespace = prefix ? prefix + "." : ""
-        return Object.keys(actions || {}).reduce(function(otherActions, name) {
+        return Object.keys(model || {}).reduce(function(otherActions, name) {
           var namedspacedName = namespace + name
-          var action = actions[name]
+          var slice = model[name]
           otherActions[name] =
-            typeof action === "function"
+            typeof slice === "function"
               ? function(data) {
-                  return function(state) {
-                    return function(actions) {
-                      var result = action(data)
-                      result =
-                        typeof result === "function" ? result(state) : result
-                      result =
-                        typeof result === "function" ? result(actions) : result
-                      options.log(
-                        state,
-                        { name: namedspacedName, data: data },
-                        result
-                      )
-                      return result
-                    }
+                  return function(store) {
+                    var result = slice(data)
+                    result =
+                      typeof result === "function" ? result(store) : result
+                    options.log(
+                      withoutFunctions(store),
+                      { name: namedspacedName, data: data },
+                      result
+                    )
+                    return result
                   }
                 }
-              : enhanceActions(action, namedspacedName)
+              : Object(slice) === slice
+                ? enhanceActions(slice, namedspacedName)
+                : slice
           return otherActions
         }, {})
       }
 
-      props.actions = enhanceActions(props.actions)
+      const enhancedModel = enhanceActions(model)
 
-      var appActions = app(props, container)
+      var appActions = app(enhancedModel, view, container)
       return appActions
     }
   }
